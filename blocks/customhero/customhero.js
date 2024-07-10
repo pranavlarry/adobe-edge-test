de665791d453bc5aa633e6bf37f37f21655c9ffc
Pranav Lari <larry@au-ki.com>
2024-07-10 19:10:02 +0530

    test

--- blocks/customhero/customhero.js
+++ blocks/customhero/customhero.js
@@ -1,9 +1,28 @@
 export default function decorate(block) {
-    const cols = [...block.firstElementChild.children];
-    console.log(cols);
     // block.textContent = '';
-    const heroImg = block.children;
-    console.log(block.children);
+    const heroImgBlock = block.children[0];
+    const boxTitleBlock = block.children[1];
+    const boxContentTopBlock = block.children[2];
+    const boxContentMiddleBlock = block.children[3];
+    const boxContentBottomBlock = block.children[4];
+    const card1Block = block.children[5];
+    const card2Block = block.children[6];
+    const card3Block = block.children[7];
+
+    const heroImge = heroImgBlock.querySelector('picture');
+    const boxTitle = boxTitleBlock.querySelector('div:last-child');
+    const boxContentTop = boxContentTopBlock.querySelector('div:last-child');
+    const boxContentMiddle = boxContentMiddleBlock.querySelector('div:last-child');
+    const boxContentBottom = boxContentBottomBlock.querySelector('div:last-child');
+    const card1Img = card1Block.querySelector('picture');
+    const card1Text = card1Block.querySelector('div:last-child');
+    const card2Img = card2Block.querySelector('picture');
+    const card2Text = card2Block.querySelector('div:last-child');
+    const card3Img = card3Block.querySelector('picture');
+    const card3Text = card3Block.querySelector('div:last-child');
+
+    console.log(heroImge, boxTitle, boxContentTop, boxContentMiddle, boxContentBottom, card1Img, card1Text, card2Img, card2Text, card3Img, card3Text);
+    // console.log(block.children);
     // block.classList.add(`columns-${cols.length}-cols`);
   
     // // setup image columns
